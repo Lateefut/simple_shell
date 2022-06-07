476af42fd18826befe58823e94a64426b73fa72e
‘samorobo’ <samgodwin111@gmail.com>
2022-06-07 14:02:41 -0700

    My shell.h commit

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef _SHELL_
+#define _SHELL_
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+
+#endif
