037ea3e03794f8cd8748fe201b8752f09254a9b1
Adebanjo Lateef <adebanjolateef18@gmail.com>
2022-06-08 01:51:27 -0700

    shell header

--- shell.h
+++ shell.h
@@ -1,6 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+extern char **environ;
+
+/* MACROS TO BE USED */
+#define BUFSIZE 1024
+#define DELIM "\t\r\n\a"
+
+/*STANDARD LIBRARIES*/
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -14,5 +21,24 @@
 #include <sys/wait.h>
 #include <sys/resource.h>
 #include <sys/time.h>
+#include <linux/limits.h>
 
+/*STRING FUNCTIONS*/
+char *_strok(char *str, const char *tok);
+unsigned int check_delim(char c, const char *str);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int _putchar(char c);
+int _atoi(char *s);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void array_rev(char *arr, int len);
+int intlen(int num);
+char *_itoa(unsigned int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
 #endif
